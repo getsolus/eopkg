1f5ec89130b1d6cd627ad26c23975824b75a9789
Ikey Doherty <ikey@solus-project.com>
2016-11-05 14:50:04 +0000

    Remove the unnecessary graph command
    This command is broken and we don't want it in sol, so drop it now
    to make sure people know it's not staying around.
    
    https://dev.solus-project.com/T377
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- pisi/cli/pisicli.py
+++ pisi/cli/pisicli.py
@@ -30,7 +30,6 @@ import pisi.cli.deletecache
 import pisi.cli.delta
 import pisi.cli.emerge
 import pisi.cli.fetch
-import pisi.cli.graph
 import pisi.cli.index
 import pisi.cli.info
 import pisi.cli.install
