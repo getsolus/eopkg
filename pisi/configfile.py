b69c93ae3e08faddcb3bfc48ab98cfa0b09a25b2
Peter O'Connor <sunnyflunk@gmail.com>
2019-01-09 12:08:33 +1100

    Change fallback to getsol.us in case it is ever used

--- pisi/configfile.py
+++ pisi/configfile.py
@@ -90,7 +90,7 @@ class BuildDefaults:
     ldflags = "-Wl,--copy-dt-needed-entries -Wl,-O1 -Wl,-z,relro -Wl,-z,now -Wl,-z,max-page-size=0x1000 -Wl,-Bsymbolic-functions -Wl,--sort-common"
     buildhelper = "ccache"
     compressionlevel = 1
-    fallback = "https://www.solus-project.com/source"
+    fallback = "https://getsol.us/sources"
     ignored_build_types = ""
 
 class DirectoriesDefaults:
