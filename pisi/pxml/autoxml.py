bd29480b80f8b04ac6babc7f55fba71809dde7bc
Joey Riches <josephriches@gmail.com>
2024-02-04 19:38:10 +0000

    pxml/autoxml: Hardcode en lang when encoding
    Issue found when porting ypkg to py3. Ensure if correct fix.

--- pisi/pxml/autoxml.py
+++ pisi/pxml/autoxml.py
@@ -88,7 +88,7 @@ class LocalText(dict):
         assert self.tag != ""
         for key in list(self.keys()):
             newnode = xmlext.addNode(node, self.tag)
-            xmlext.setNodeAttribute(newnode, "xml:lang", key)
+            xmlext.setNodeAttribute(newnode, "en", key)
             xmlext.addText(newnode, "", self[key])
 
     # FIXME: maybe more appropriate for pisi.util
