4d457838937137420dce3f3af485b06c24c4e0b1
Joey Riches <josephriches@gmail.com>
2024-04-14 17:16:10 +0100

    Revert "archive.py: Re-encode file_name object to utf-8"
    This reverts commit e997a06360e110da1fc93274c9134b502df15b57.
    
    Unneccessary after https://github.com/getsolus/ypkg/pull/70

--- pisi/archive.py
+++ pisi/archive.py
@@ -450,9 +450,8 @@ class ArchiveTar(ArchiveBase):
             pass
         self.close()
 
-    def add_to_archive(self, file_name: bytes, arc_name=None):
+    def add_to_archive(self, file_name, arc_name=None):
         """Add file or directory path to the tar archive"""
-
         if not self.tar:
             if self.type == "tar":
                 wmode = "w:"
@@ -476,10 +475,7 @@ class ArchiveTar(ArchiveBase):
             if self.tar is None:
                 self.tar = tarfile.open(self.file_path, wmode, fileobj=self.fileobj)
 
-        # py3 needs strings encoded to utf-8, so decode the bytestream to latin-1 first
-        # and then re-encode it as an utf-8 string for the tarfile.py py3 library.
-        # This bug was exposed by the usdx package.yml
-        self.tar.add(file_name.decode("latin-1").encode("utf-8"), arc_name)
+        self.tar.add(file_name.decode("latin-1"), arc_name)
 
     def close(self):
         self.tar.close()
