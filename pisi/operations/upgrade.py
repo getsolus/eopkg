10caf19ac342d0e0ae2da0c42e70dda0dc746e84
Rune Morling <ermo@serpentos.com>
2024-06-19 23:15:15 +0200

    Force install/upgrade order for eopkg & baselayout
    This will ensure that future upgrades to eopkg and baselayout will always
    happen in a deterministic fashion.
    
    If eopkg is part of the install or upgrade transaction, eopkg will be
    moved to the front of the transaction.
    
    If baselayout is part of the install or upgrade transaction, baselayout
    will be moved to the front of the transaction (possibly in front of
    eopkg).
    
    Note that testing this commit requires you to do a system wide pip
    install until we get the ordered-set package included in the repo:
    
        sudo pip install ordered-set
    
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- pisi/operations/upgrade.py
+++ pisi/operations/upgrade.py
@@ -12,6 +12,7 @@
 
 import sys
 
+from ordered_set import OrderedSet as set
 from pisi import translate as _
 
 import pisi
@@ -334,6 +335,7 @@ def plan_upgrade(A, force_replaced=True, replaces=None):
         G_f.write_graphviz(sys.stdout)
 
     order = G_f.topological_sort()
+    order = operations.install.plan_deterministic_install_order(order)
     order.reverse()
     return G_f, order
 
@@ -366,8 +368,14 @@ def upgrade_base(A = set()):
                                  "following packages:"))
                 ctx.ui.info(util.format_by_columns(sorted(extra_upgrades)))
                 G_f, upgrade_order = plan_upgrade(extra_upgrades, force_replaced=False)
+            install_and_upgrade_order = set(install_order + upgrade_order)
+            if len(install_and_upgrade_order) > 1 and ctx.config.get_option("debug"):
+                ctx.ui.info(_("installs and upgrades (unordered): %s" % install_and_upgrade_order))
+            install_and_upgrade_order = operations.install.plan_deterministic_install_order(install_and_upgrade_order)
+            if len(install_and_upgrade_order) > 1 and ctx.config.get_option("debug"):
+                ctx.ui.info(_("installs and upgrades (deterministic order): %s" % install_and_upgrade_order))
             # return packages that must be added to any installation
-            return set(install_order + upgrade_order)
+            return install_and_upgrade_order
         else:
             ctx.ui.warning(_('Safety switch: The component system.base cannot be found.'))
     return set()
