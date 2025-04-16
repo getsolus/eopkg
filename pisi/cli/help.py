65996a418edfca6612fad335921c484caa104990
Hans K <hans@communitycomputing.net>
2025-04-15 23:21:39 -0400

    pisi.cli.help: Import all commands that sort alphabetically after help
    We need to force-import all of these or help is truncated due to how pisi.command.autocommand is written.
    
    Fixes #50.

--- pisi/cli/help.py
+++ pisi/cli/help.py
@@ -7,6 +7,30 @@ import pisi.cli
 import pisi.cli.command as command
 import pisi.context as ctx
 
+# Import all the commands that are alphabetically after help
+# We need to force-import all of these or help is truncated due to how pisi.command.autocommand is written.
+# TODO: Modernize the entire CLI and remove this nonsense.
+from pisi.cli.history import History
+from pisi.cli.index import Index
+from pisi.cli.info import Info
+from pisi.cli.install import Install
+from pisi.cli.listavailable import ListAvailable
+from pisi.cli.listcomponents import ListComponents
+from pisi.cli.listinstalled import ListInstalled
+from pisi.cli.listnewest import ListNewest
+from pisi.cli.listpending import ListPending
+from pisi.cli.listrepo import ListRepo
+from pisi.cli.listupgrades import ListUpgrades
+from pisi.cli.rebuilddb import RebuildDb
+from pisi.cli.remove import Remove
+from pisi.cli.removeorphans import RemoveOrphans
+from pisi.cli.removerepo import RemoveRepo
+from pisi.cli.repopriority import RepoPriority
+from pisi.cli.search import Search
+from pisi.cli.searchfile import SearchFile
+from pisi.cli.updaterepo import UpdateRepo
+from pisi.cli.upgrade import Upgrade
+
 
 class Help(command.Command, metaclass=command.autocommand):
     __doc__ = _(
