51a82ccfa76e50afb6bfed556bc5db3e81d5491e
Joey Riches <josephriches@gmail.com>
2026-05-28 12:19:07 +0100

    fetcher: Add support for fetch_multi
    Fetches multiple items concurrently

--- pisi/fetcher.py
+++ pisi/fetcher.py
@@ -3,16 +3,15 @@
 
 import os
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
 from requests import HTTPError
 from requests.adapters import HTTPAdapter
-
 from tqdm import tqdm
 
 import pisi
 import pisi.context as ctx
-
 from pisi import translate as _
 from pisi.uri import URI
 
@@ -48,12 +47,15 @@ class Fetcher:
         proxies = self._get_proxies()
         self.session.proxies.update(proxies)
 
-    def download_file(self, url: str, destination: str) -> None:
+    def download_file(
+        self, url: str, destination: str, progress_bar_pos: int = 0
+    ) -> None:
         """
         Download a remote resource to a local file.
 
         :param str url: The URL of the resource to download.
         :param str destination: The destination file to download to.
+        :param int progress_bar_pos: The position of the progress bar.
         """
         with self.session.get(url.get_uri(), stream=True, timeout=15) as resp:
             resp.raise_for_status()
@@ -69,6 +71,7 @@ class Fetcher:
                     unit="iB",
                     unit_scale=True,
                     unit_divisor=1024,
+                    position=progress_bar_pos,
                 ) as bar,
             ):
                 for chunk in resp.iter_content(chunk_size=MAX_CHUNK_SIZE):
@@ -90,7 +93,13 @@ class Fetcher:
                         if elapsed < expected_time:
                             time.sleep(expected_time - elapsed)
 
-    def fetch(self, url: URI | str, dest_dir: str, filename: str = None) -> None:
+    def fetch(
+        self,
+        url: URI | str,
+        dest_dir: str,
+        filename: str | None,
+        progress_bar_pos: int = 0,
+    ) -> None:
         """
         Fetches a remote resource.
 
@@ -99,6 +108,7 @@ class Fetcher:
         :param str dest_dir: The directory to save the downloaded file to.
         :param filename: The name of the file to use.
         :type filename: str | None
+        :param int progress_bar_pos: The position of the progress bar.
         """
         # This is silly and I hate it.
         if type(url) is str:
@@ -116,9 +126,44 @@ class Fetcher:
             raise IOError(_(f"Unable to access destination file '{archive_file}'"))
 
         try:
-            self.download_file(url, archive_file)
+            self.download_file(url, archive_file, progress_bar_pos)
         except HTTPError:
             raise
+
+    def fetch_multi(self, items: list) -> None:
+        """
+        Fetches multiple remote resources concurrently.
+
+        :param items: A list of (url, dest_dir, filename) tuples.
+        """
+
+        # TODO(Joey): Add a seperate config value for this
+        max_workers = pisi.util.parse_jobs(ctx.config.values.build.jobs)
+        if max_workers == 0:
+            max_workers = 8
+        # Cap the number of concurrent downloads to 8 to avoid overloading
+        if max_workers > 8:
+            max_workers = 8
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = []
+
+            for i, item in enumerate(items):
+                url, dest_dir, *rest = item
+                filename = rest[0] if rest else None
+
+                futures.append(
+                    executor.submit(
+                        self.fetch, url, dest_dir, filename, i % max_workers
+                    )
+                )
+
+            for future in futures:
+                try:
+                    future.result()
+                except Exception as e:
+                    ctx.ui.error(str(e))
+                    raise
 
     def _get_bandwidth_limit(self) -> int:
         bandwidth_limit = (
